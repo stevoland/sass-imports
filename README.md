dcb8c0ae080eb6266d858785ac356b3eb11d205e
stevo <stevoland@gmail.com>
2016-01-28 11:20:15 +0000

    New API, cleanup and fix hot-reloading dependencies

--- README.md
+++ README.md
@@ -1,29 +1,53 @@
-#webpack loader for import json as sass variables
+# sass-vars-loader
 
-##converts json to sass veriables
-####example:
-*test.json*
-```json
-{
-  "a": {
-    "b": "100px",
-    "c": {
-      "d": 99
+## Webpack loader to import JavaScript object as Sass variables
+
+Convert a JavaScript object to Sass variables and prepend them to your Sass files.
+Useful for theming and sharing values between JavaScript and Sass for those who don't want
+to go all in on CSS in JS.
+
+### Example:
+
+```javascript
+// You can require other modules but for hot-reloading to work they should be
+// within the `context` dir
+var aValue = require('./other-module');
+
+module.exports = {
+  a: {
+    b: '100px',
+    c: {
+      d: 99,
+      e: true,
+      f: aValue
     }
-  }
-}
+  },
+  // URLs to assets should be relative to your Webpack `resolve.root`
+  // so they can be `required` from any JS or Sass module
+  logo: 'themes/id/logo.svg'
+};
 ```
 
-will be converted to
+Will be converted to:
+
 ```sass
-  $a-b : 100px;
-  $a-c-d : 99;
+  $a-b: 100px;
+  $a-c-d: 99;
+  $a-c-e: true;
+  $a-c-f: 1em; // From other-module';
+  $logo: "themes/id/logo.svg";
 ```
+
+Values containing `/` are wrapped with quotes, otherwise they become Sass values.
 
 ## Usage
+```javascript
+'style-loader!css-loader!autoprefixer-loader!sass-loader?indentedSyntax=sass!sass-imports?context=' + path.resolve(__dirname, 'themes') + '&file=id/vars.js'
 ```
-style-loader!css-loader!autoprefixer-loader!sass-loader?indentedSyntax=sass!sass-imports?./common_vars.json
-```
+
+ - `context` - Absolute path to a folder. Changes to assets in this folder trigger rebuilds
+ - `file` - Path to module that exports vars object. Absolute or relative to `context`
+ - `files[]` - Array of `file`s
 
 ## License
 MIT (http://www.opensource.org/licenses/mit-license.php)
--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-/*
-  MIT License http://www.opensource.org/licenses/mit-license.php
-  Author istarkov@gmail.com
-  converts json to sass veriables
-  example:
-  test.json
-  ```
-  {
-    "a": {
-      "b": '100px',
-      "c": {
-        "d": 99
-      }
-    }
-  }
-  ```
-
-  will be converted to
-  ```
-    $a-b : 100px
-    $a-c-d : 99
-  ```
-*/
 var loaderUtils = require('loader-utils');
 var path = require('path');
 var traverse = require('traverse');
@@ -30,28 +7,66 @@ var URL_REGEX = /\//;
 
 module.exports = function SassImports(content) {
   var query = loaderUtils.parseQuery(this.query);
+  var context;
+  var files = [];
   var imports = [];
   var dir = path.dirname(path.resolve(this.resourcePath));
   var self = this;
   var prefix = '';
   if (this.cacheable) this.cacheable();
 
-  Object.keys(query).forEach(function traverseFiles(fileName) {
-    // var fpath = path.join(dir, fileName);
-    // var includeContent = JSON.parse(fs.readFileSync(fpath));
-    var code = fs.readFileSync(fileName, {encoding: 'utf8'});
-    var includeContent = self.exec(code, fileName);
+  if (query.file) {
+    if (Array.isArray(query.file)) {
+      files = files.concat(query.file);
+    } else {
+      files.push(query.file);
+    }
+  }
+
+  if (query.files) {
+    if (Array.isArray(query.files)) {
+      files = files.concat(query.files);
+    } else {
+      files.push(query.files);
+    }
+  }
+
+  if (query.context) {
+    context = query.context;
+    self.addContextDependency(context);
+  }
+
+  var requireFresh = require('require-fresh')({
+    dir: context,
+    watch: false,
+    force: true
+  });
+
+  files.forEach(function traverseFiles(fileName) {
+    // var code = fs.readFileSync(fileName, {encoding: 'utf8'});
+    // var includeContent = self.exec(code, fileName);
+    var absoluteFilename = fileName;
+    if (context) {
+      absoluteFilename = path.join(context, absoluteFilename);
+    }
+
+    var relativeFilename = path.relative(dir, fileName);
+    var includeContent = requireFresh(fileName, {fresh: true});
     var obj = traverse(includeContent);
 
-    self.addDependency(fileName);
+    self.addDependency(relativeFilename);
 
     obj.paths().forEach(function traverseJSONPAth(jsonPath) {
       var val = obj.get(jsonPath);
+
       if (typeof val !== 'object') {
         if (typeof val === 'string' && URL_REGEX.test(val)) {
           val = '"' + val + '"';
+        } else if (typeof val === 'boolean') {
+          val = val ? 'true' : false;
         }
-        imports.push('$'+jsonPath.join('-') + ': ' + val + ';');
+
+        imports.push('$' + jsonPath.join('-') + ': ' + val + ';');
       }
     });
   });
